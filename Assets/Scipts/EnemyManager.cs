af0a66b021020c015349cc8b74e8271aa8ec1440
Hasan ÖZSOY <hasanzsoy@outlook.com>
2026-04-24 21:22:39 +0300

    space tuşuna basınca karakter oluşturmayı update e alındı startta doğru çalışmıyor

--- Assets/Scipts/EnemyManager.cs
+++ Assets/Scipts/EnemyManager.cs
@@ -10,12 +10,7 @@ public class EnemyManager : MonoBehaviour
         //Instantiate(enemyPrefab, new Vector3(5, 20, 4), transform.rotation); // transform.rotation, objenin rotasyonunu, EnemyManager objesinin rotasyonuna göre oluşturur, yani EnemyManager objesi döndürülmüşse, oluşturulan objede aynı şekilde döndürülür
         //Instantiate(enemyPrefab, new Vector3(0, 0.97f, 0), Quaternion.identity); // Quaternion.identity, objenin rotasyonunu sıfırlar, yani objenin kendi eksenlerine göre döndürülmemiş bir şekilde oluşturulmasını sağlar
         //Instantiate(enemyPrefab, new Vector3(2,3, 0), Quaternion.identity);
-        
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        {
-            Instantiate(enemyPrefab, new Vector3(0, 0.97f, 0), transform.rotation);
-        }
-
+       
         for (int i = 0; i < 5; i++)
         {
             Vector3 randomPosition = new Vector3(Random.Range(-5f, 5f), 0.97f, Random.Range(-5f, 5f));
@@ -23,6 +18,14 @@ public class EnemyManager : MonoBehaviour
             // her frame'de rastgele bir pozisyonda düşman oluşturur,
             // bu yüzden çok fazla düşman oluşabilir ve performans sorunlarına yol açabilir,
             // bu yüzden bu kodu dikkatli kullanmak gerekir
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(enemyPrefab, new Vector3(0, 0.97f, 0), transform.rotation);
         }
     }
 }
