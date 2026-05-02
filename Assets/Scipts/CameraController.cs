80fcf0286bb59e2c4b9208f1b8a1c58d95b88e10
Hasan ÖZSOY <hasanzsoy@outlook.com>
2026-05-02 18:51:58 +0300

    Kamera Takip x ekseni sabit kalan kod

--- Assets/Scipts/CameraController.cs
+++ Assets/Scipts/CameraController.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Vector3 offset;
+   
+    private void LateUpdate()
+    {
+        FollowPlayerPosition();
+    }
+
+    private void FollowPlayerPosition()
+    {
+        Vector3 newPosition = transform.position = playerTransform.position + offset;
+        newPosition.x = 0;
+        transform.position = newPosition;
+    }
 }
